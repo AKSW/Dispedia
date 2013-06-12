27aac2a0772fbee6d0008903efa04b804ec6e84b
Lars Eidam <larseidam@googlemail.com>
2013-06-12 17:27:00 +0200

    update eHealthServices2013 Website
    - finsh layout
    - add content data
    - add pictures
    - update eHealthServices2013 ontology
    - fixes #44

--- htdocs/ehealthservices2013/js/script.js
+++ htdocs/ehealthservices2013/js/script.js
@@ -2,22 +2,9 @@ $(document).ready(function() {
     $("li.active").prepend('<canvas id="canvasCircle" width="' + $("li.active").css('width') + '" height="' + $("li.active").css('height') + '"></canvas>');
     
     $("canvas#canvasCircle").drawArc({
-        fillStyle: "white",
+        fillStyle: "#21576b",
         x: ($("li.active canvas").width()+1)/2, y: 0,
         radius: 4
     });
-    $("canvas#canvasCircle").drawArc({
-        fillStyle: "white",
-        x: ($("li.active canvas").width()+1)/2, y: $("li.active canvas").height(),
-        radius: 4
-    });
     
-    $(".schemaContent .span9 .row canvas").drawLine({
-        strokeStyle: "#00A3C6",
-        strokeWidth: 3 ,
-        rounded: true,
-        x1: $(".schemaContent .span9 .row canvas").width()*1/4, y1: $(".schemaContent .span9 .row canvas").height()*5/8,
-        x2: $(".schemaContent .span9 .row canvas").width()/2, y2: $(".schemaContent .span9 .row canvas").height()*3/8,
-        x3: $(".schemaContent .span9 .row canvas").width()*3/4, y3: $(".schemaContent .span9 .row canvas").height()*5/8
-    });
 });
